e5cb80cbe7559b44a51f3147b0107d4c620a1a14
fabriciogomes1898-a11y <fabriciogomes1898@gmail.com>
2026-05-23 17:31:37 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'jordan-barber-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/manifest.json',
+    '/favicon.png',
+    '/icon-192.png',
+    '/icon-512.png',
+    '/copy-paste-bros.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+});
